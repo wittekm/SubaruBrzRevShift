476d9635283ce1c4a902e4019e35fd1ff525d3e3
Max Wittek <wimax@dropbox.com>
2020-07-16 15:43:59 -0700

    About to switch to imperial, gross

--- Odb2/gear_find.hpp
+++ Odb2/gear_find.hpp
@@ -17,21 +17,50 @@ static float GEAR_RATIOS[] = {
     0.767,
 };
 
-static float TIRE_RADIUS_CM = 62.738 / 2.0;
-static float TIRE_CIRCUMFERENCE_M = 2.0 * PI * TIRE_RADIUS_CM / 1000.0;
+static float CM_TO_M = 1.0/100.0;
+static float TIRE_DIAMETER_CM = 62.738;
+static float TIRE_DIAMETER_M = 62.738 * CM_TO_M;
+static float TIRE_RADIUS_CM = TIRE_DIAMETER_CM / 2.0;
+static float TIRE_RADIUS_M = TIRE_RADIUS_CM * CM_TO_M;
+static float TIRE_CIRCUMFERENCE_M = 2.0 * PI * TIRE_RADIUS_M;
 
 static float FINAL_RATIO = 4.10;
+static float MAGIC_CONSTANT = 336.13524;
 
+/*
+https://www.dropbox.com/s/o2ce1x7ddnhqueg/Screen%20Shot%202020-07-16%20at%203.18.34%20PM.png?dl=0
+https://www.rcnmag.com/tech/calculating-mph-rpm-gear-ratio-tire-size
+https://www.rcnmag.com/resources/images/RPMMPHGear-Ratio-1.jpg
+336 magic constant
+*/
 
 float rpmAtGear(float speed, int gear) {
     // mph * (gear * final) / circ = rpm
     float expectedRpm = float(speed) * (GEAR_RATIOS[gear] * FINAL_RATIO) / TIRE_CIRCUMFERENCE_M;
     return expectedRpm;
+
+    float numer = (speed * )
 }
 
 float speedAtGear(float rpm, int gear) {
+    float expected = rpm * TIRE_DIAMETER_M / (GEAR_RATIOS[gear] * FINAL_RATIO * MAGIC_CONSTANT);
+    return expected;
+}
+
+float speedAtGearOG(float rpm, int gear) {
     // this is in kph, but the formula is:
     // mph = (rpm * cir) / (gear * final) 
+    //.... or so i thought
+    /**
+     * ans1 rpm
+     * ans2 TIRE_DIAMETER_CM
+     * ans3 rear gear ratio
+     * ans4 final ratio
+     * MPH = eval( Math.floor( ( (ans1 * ans2 ) / (ans3 * ans4 * 336.0) ) * 1000.0 + 0.5 ) / 1000.0 )
+     *                                                                                             vv MPH to KMH 
+     f.result2.value = ( ( (rpm * diameter_INCH ) / (gear * final * 336.0) ) * 1.609344 )
+     */
+
     float expectedSpeed = float(rpm) * TIRE_CIRCUMFERENCE_M / (GEAR_RATIOS[gear] * FINAL_RATIO);
     return expectedSpeed;
 }
--- Odb2/tests.cpp
+++ Odb2/tests.cpp
@@ -9,13 +9,15 @@ int main()
     /**
      *  RPM: 7800
      *  Final Drive Ratio: 4.100
-     * 1st Gear (mph): 38
         1st Gear (kmh): 61
-        2nd Gear (mph): 64
         2nd Gear (kmh): 102
         */
-    float gearMatch = closestGearMatch(102, 7800);
-    float rpmExpected = rpmAtGear(102, 2); // it should be 7800...
+    //float gearMatch = closestGearMatch(102, 7800);
+    float gearMatch = 1;
+    float rpmExpected = 0;//rpmAtGear(102, 2); // it should be 7800...
+
+    float expect62 = speedAtGear(7800, 2);
+    cout << "Expected 62" << expect62 << endl;
 
     cout << "Best gear guess (should be gear2)" << gearMatch << endl 
         << "rpm expected (ideally 7800)" << rpmExpected << endl;
